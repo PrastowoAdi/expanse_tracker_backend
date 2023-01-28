afc3e1fcfd52939029b9369725eef7d294d125e5
prastowoadi18 <prastowoadiwork@gmail.com>
2023-01-28 11:33:31 +0700

    Setup mongoose

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,6 @@
 const routes = require("express").Router();
+const controller = require("../controller/controller");
 
-routes
-  .route("/api/categories")
-  .get((req, res) => res.json("Get Request from Categories"));
+routes.route("/api/categories").get(controller.create_Categories);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -2,15 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = 5000;
+require("dotenv").config({ path: "./config.env" });
+
+const port = process.env.PORT || 6000;
 
 //use middleware
 app.use(cors());
 app.use(express.json());
 
+//mongodb connection
+const conn = require("./db/connection");
+
 //using routes
 app.use(require("./routes/route"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+conn
+  .then((db) => {
+    if (!db) return process.exit(1);
+
+    app.listen(port, () => {
+      console.log(`Server is running on port: http://localhost/${port}`);
+    });
+
+    app.on("error", (err) =>
+      console.log(`Failed To Connect with HTTP Server: ${err}`)
+    );
+
+    //error in mongodb connection
+  })
+  .catch((error) => {
+    console.log(`Connection Failed...! ${error}`);
+  });
