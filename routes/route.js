368136ade5bda7615aa3a8b13ec18de8e72983b7
prastowoadi18 <prastowoadiwork@gmail.com>
2023-01-29 07:25:36 +0700

    Create action categories and transaction

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,16 @@
 const routes = require("express").Router();
 const controller = require("../controller/controller");
 
-routes.route("/api/categories").get(controller.create_Categories);
+routes
+  .route("/api/categories")
+  .post(controller.create_Categories)
+  .get(controller.get_Categories);
 
+routes
+  .route("/api/transaction")
+  .get(controller.get_Transaction)
+  .post(controller.create_Transaction)
+  .delete(controller.delete_Transaction);
+
+routes.route("/api/labels").get(controller.get_Labels);
 module.exports = routes;
